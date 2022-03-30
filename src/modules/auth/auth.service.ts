54d564b2cdb89e843ed0e83902b89961a2be6594
Pablo Villalobos <pablovillalobos.ing@gmail.com>
2022-03-30 15:58:20 -0300

    service encoder

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,17 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { RegisterUserDTO } from './dto/register-user.dto';
+import { EncoderService } from './encoder.service';
 import { UserRepository } from './user.repository';
 
 @Injectable()
 export class AuthService {
     constructor(
         @InjectRepository(UserRepository)
-        private userRepo: UserRepository
+        private userRepo: UserRepository,
+        private encodeService: EncoderService
     ) { }
 
     async registerUser(newUser: RegisterUserDTO): Promise<void> {
-        return  this.userRepo.createUser(newUser);
+        const {nombre, mail, password} = newUser;
+        const hashedPassword = await this.encodeService.encodePassword(password);
+        return  this.userRepo.createUser(nombre, mail, hashedPassword);
         
     }
 }
--- src/modules/auth/user.repository.ts
+++ src/modules/auth/user.repository.ts
@@ -6,20 +6,17 @@ import * as bcrypt from 'bcrypt';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
-    async createUser(newUser: RegisterUserDTO): Promise<void> {        
-        const { nombre, mail, password } = newUser
-        const salt = await bcrypt.genSalt();
-        const hashedPassword = await bcrypt.hash(password, salt);
-        const user = this.create({ nombre, mail, password: hashedPassword });
+    async createUser(nombre: string, mail: string, password: string): Promise<void> {
+        const user = this.create({ nombre, mail, password });
         try {
             await this.save(user);
         } catch (e) {
             if (e.code === 'ER_DUP_ENTRY') {
-                throw new ConflictException('Email ya registrado');                
+                throw new ConflictException('Email ya registrado');
             }
             throw new InternalServerErrorException();
         }
-        
+
 
     }
 }
